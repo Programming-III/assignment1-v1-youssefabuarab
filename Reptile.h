cccedf763b2d570c7492fcb95adc0c5e8f78f1c4
youssefabuarab <youssefabuarab6@gmail.com>
2025-11-12 15:47:26 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,26 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+#include <iostream>
+using namespace std;
+#include <string>
 
-#include "Animal.h"
-
-//define reptile class here
-
-
-
+#ifndef Reptile_H
+#define Reptile_H
+#include "Animal_H"
 
 
+class Reptile_H : public Animal {
+    private :
+    bool isVenomous  ;
+    
+    public : 
+    Reptile_H ();
+    
+    
+    
+    float wingSpan getwingspan();
+    void setwingSpan ();
+    
 
-
-
+    void display();
+};
 
 #endif

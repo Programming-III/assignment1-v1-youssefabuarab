0902d40ac1892b6a00914dfb581402254468a1e3
youssefabuarab <youssefabuarab6@gmail.com>
2025-11-12 15:48:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
+#include "Animal_h"
+#include "Bird_h"
+#include "Mammal_h"
+#include "Reptile_h"
+#include "Enclosure_h"
+#include "Visitor_h"
 #include <iostream>
 using namespace std;
 
+class Animal {
+ 
+    public : 
+    void display( );
+    void feed( );
+    
+    
+}
 
-// ============== MAIN FUNCTION ==============
-int main() {
+
+
+class Enclosure {
     
-    
+}
+
+
+
+int main()
+{
+   cout<<"";
+
     return 0;
 }

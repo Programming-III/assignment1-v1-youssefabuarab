6bbbbe783a62c05eea13698626d738163c0277a9
youssefabuarab <youssefabuarab6@gmail.com>
2025-11-12 15:40:31 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,24 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+#include <iostream>
+using namespace std;
+#include <string>
 
-#include "Animal.h"
-
-//define mammal class here
-
-
-
+#ifndef Mammel_H
+#define Mammel_H
+#include "Animal_H"
 
 
+class Mammal : public Animal {
+private:
+    string furColor;
 
+public:
+    Mammal();
+    Mammal(string name, int age, bool isHungry, string furColor);
+
+    string getFurColor() const;
+    void setFurColor(string color);
+
+
+    void display();
+};
 #endif
